3db4718aace6cdd583c180a3650b99717a66308a
Sammar Hayat <sammarhayat1122@gmail.com>
2024-05-06 21:08:57 +0500

    error handling

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const app = express();
+const errorMiddleware = require("./middleware/error.js")
 
 app.use(express.json())
 
@@ -7,5 +8,7 @@ app.use(express.json())
 const product = require("./routes/productRoute")
 
 app.use("/api/v1",product)
+
+app.use(errorMiddleware)
 
 module.exports = app;
