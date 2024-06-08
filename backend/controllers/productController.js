34c6375937f7d6880108c43b639a14e158b21d91
Sammar Hayat <sammarhayat1122@gmail.com>
2024-06-08 13:56:55 +0500

    search, filter, pagiation

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,7 @@
 const Product = require("../models/productModel");
 const ErrorHandler = require("../utils/errorHandler");
 const catchAsyncErrors = require("../middleware/catchAsyncErrors");
+const ApiFeatures = require("../utils/apifeatures");
 
 // create product --Admin
 exports.createProduct = catchAsyncErrors(async (req, res, next) => {
@@ -13,10 +14,13 @@ exports.createProduct = catchAsyncErrors(async (req, res, next) => {
 
 // get all products
 exports.getAllProducts = catchAsyncErrors(async (req, res) => {
-  const products = await Product.find();
+  let count =await Product.countDocuments()
+  const apiFeature = new ApiFeatures(Product.find(), req.query).search().filter().pagination();
+  const products = await apiFeature.query;
   res.status(200).json({
     success: true,
     products,
+    count
   });
 });
 
@@ -30,7 +34,7 @@ exports.getProductDetails = catchAsyncErrors(async (req, res, next) => {
 
   res.status(200).json({
     success: true,
-    product,
+    product
   });
 });
 
@@ -39,7 +43,7 @@ exports.updateProduct = catchAsyncErrors(async (req, res, next) => {
   let product = await Product.findById(req.params.id);
 
   if (!product) {
-    return next(new ErrorHandler("Product not found", 404))
+    return next(new ErrorHandler("Product not found", 404));
   }
   product = await Product.findByIdAndUpdate(req.params.id, req.body, {
     new: true,
