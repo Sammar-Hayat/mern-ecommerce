a9f295fbeb79a7a3eb3aa90b01a013619916096a
Sammar Hayat <sammarhayat1122@gmail.com>
2024-06-11 15:01:54 +0500

    jwt

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -14,8 +14,36 @@ exports.registerUser = catchAsyncErrors(async (req, res, next) => {
       url: "profilePicUrl",
     },
   });
+
+  const token = user.getJWTToken();
+
   res.status(201).json({
     success: true,
-    user,
+    token,
+  });
+});
+
+//logi user
+exports.loginUser = catchAsyncErrors(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  if (!email || !password) {
+    return next(new ErrorHandler("Please enter Email & Password", 400));
+  }
+
+  const user = await User.findOne({ email }).select("+password");
+
+  if (!user) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+  const isPasswordMatched = await user.comparePassword(password);
+  if (!isPasswordMatched) {
+    return next(new ErrorHandler("Invalid Email or Password", 401));
+  }
+  const token = user.getJWTToken();
+
+  res.status(200).json({
+    success: true,
+    token,
   });
 });
--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,7 @@
 const mongoose = require("mongoose");
 const validator = require("validator");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
 
 const userSchema = mongoose.Schema({
   name: {
@@ -38,4 +40,24 @@ const userSchema = mongoose.Schema({
   resetPasswordExpire: Date,
 });
 
+// hash password
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) {
+    next();
+  }
+  this.password = await bcrypt.hash(this.password, 10);
+});
+
+//JWT Token Generate
+userSchema.methods.getJWTToken = function () {
+  return jwt.sign({ id: this._id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRE,
+  });
+};
+
+// JWT compare
+userSchema.methods.comparePassword = async function (enteredPassword) {
+  return await bcrypt.compare(enteredPassword,this.password)
+};
+//
 module.exports = mongoose.model("User", userSchema);
